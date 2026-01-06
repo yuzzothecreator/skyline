de2c4165ea2c372dff16db3aca382ef682cb4fe9
yuzzothecreator <yuzzothecreator@gmail.com>
2026-01-06 03:38:03 -0800

    solve the import error

--- client/src/components/layout/Footer.tsx
+++ client/src/components/layout/Footer.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import { Mail, Heart, Facebook, Instagram,  } from 'lucide-react'
+import { Mail, Heart  } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 
 const Footer = () => {
